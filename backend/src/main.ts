e325620b55d93aca6bd06f221583f6fc0903d598
Anucha Prapphrom <anucha.21681@hotmail.com>
2020-03-30 22:10:46 +0700

    feat(system): Update config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,23 @@ import { NestFactory } from '@nestjs/core';
 import * as swaggerUi from 'swagger-ui-express';
 import * as YAML from 'yamljs';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const privateKey = fs.readFileSync(path.resolve(__dirname, './../server-key.pem'));
+  const certificate = fs.readFileSync(path.resolve(__dirname, './../server-crt.pem'));
+  const ca = fs.readFileSync(path.resolve(__dirname, './../ca-crt.pem'));
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: privateKey,
+      cert: certificate,
+      ca,
+    },
+    cors: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   const swaggerDocument = YAML.load('./swagger.yaml');
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
